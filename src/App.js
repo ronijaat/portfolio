874f2d6e952cc11903710de15ab5f5c4849e5374
ronijaat <raunakbaliyan01@gmail.com>
2023-09-21 12:19:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { About,Footer,Header,Skills,Testimonial,Work } from "./container";
+import {Navbar} from './components';
+
+import './App.scss'
+import './assets/react-tooltip.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header/>
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BsLinkedin, BsGithub } from 'react-icons/bs';
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <a href="https://github.com/ronijaat" target="_blank"><BsGithub /></a>
+      
+    </div>
+    <div>
+    <a href="https://www.linkedin.com/in/raunak-baliyan-18448b201/" target="_blank"><BsLinkedin /></a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
